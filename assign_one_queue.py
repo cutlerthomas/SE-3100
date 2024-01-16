8fa5d0ddb07596d11fc15685f043876afe4366dc
cutlerthomas <cutlerthomas0@gmail.com>
2024-01-16 14:02:57 -0700

    update

--- assign_one_queue.py
+++ assign_one_queue.py
@@ -1,3 +1,19 @@
 
 
 
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+    
+    def get_front(self):
+        return self.queue.pop(0)
+    
+    def get_back(self):
+        return self.queue.pop(len(self.queue) - 1)
+    
+    def insert_back(self, item):
+        self.queue.append(item)
+
+    def insert_front(self, item):
+        self.queue.insert(0, item)
